d834b2394d11a1b5574d2176e619be339e5b6c63
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-01-31 11:20:22 +0600

    push,pop,shift,unshift.

--- hello.js
+++ hello.js
@@ -5,3 +5,29 @@ console.log(friend);
 console.log(friend.length)
 var oneroll = friend.indexOf(21);
 console.log(oneroll);
+friend[3]=50;
+console.log(friend);
+friend[4]=50;
+console.log(friend);
+friend.push(23)
+console.log(friend);
+friend.pop(23)
+console.log(friend);
+friend.unshift(3)
+friend.push(23)
+////last array value added
+console.log(friend);
+friend.pop()
+//last array value remove
+console.log(friend);
+friend.shift()
+//frist array value remove
+console.log(friend);
+
+friend.unshift(3)
+//frist array value added
+console.log(friend);
+
+
+
+
